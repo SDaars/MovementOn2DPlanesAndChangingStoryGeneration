3d93ac8f5deae938bdee73706840424afa845aed
shane <shane@SNACCBOOK-XL>
2021-10-18 20:18:42 +1100

    Add project files.

---
+++ MovementOn2DPlanesAndChangingStoryGeneration/MovementPlane.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace MovementOn2DPlanesAndChangingStoryGeneration
+{
+    class MovementPlane
+    {
+        const int BoardSize = 11;
+        //Movement planes
+        int x { get; set; } = 6;
+        int y { get; set; } = 6;
+
+        enum TerrainTypes {
+            Grassland,
+            Forest,
+            Desert,
+            Tundra,
+            City,
+            Swamp,
+            Blight,
+            Pixie
+        }
+
+
+        public void BoardGeneration(string name)
+        {
+            //Generating Terrain
+            //Possible Terrains: Grassland, Forest, Desert, Tundra, City, Swamp, Blight, Pixie
+            using (StreamWriter BoardStream = new StreamWriter("GameBoard.txt"))
+            {
+                Random ranterrain = new Random();
+                int interval = 0;
+                while (interval < BoardSize)
+                {
+                    for (int i = 0; i < BoardSize; i++)
+                    {
+                        int randnum = ranterrain.Next(7);
+                        BoardStream.Write(randnum);
+                    }
+                    BoardStream.Write("\n");
+                    interval++;
+                }
+            }
+
+            Console.WriteLine("This is the gameboard:");
+            using (StreamReader BoardReader = new StreamReader("GameBoard.txt"))
+            {
+                int count = 0;
+                string line;
+                while ((line = BoardReader.ReadLine()) != null)
+                {
+                    count++;
+                    Console.WriteLine(line); 
+                }
+            }
+
+        }
+
+        private int TerrainChecker()
+        {
+            //Will check the terrain and return an int for the other methods to use. 
+            using (StreamReader BoardReader = new StreamReader("GameBoard.txt"))
+            {
+                for (int i = 1; i < y; i++)
+                {
+                    BoardReader.ReadLine();
+                }
+                string boardrow = BoardReader.ReadLine();
+
+                Console.WriteLine(boardrow[(x - 1)]);
+                return boardrow[x];
+            }
+        }
+
+        private void StoryGeneration(int terrain)
+        {
+            //Will grab a random story according to the terrain and give options. Will interact with inventory.
+
+            Console.WriteLine("You fall over and die.\nItem recieved: Bones");
+        }
+
+        public void Movement(char direction) //out string story, out char storytype)
+        {
+            //Movement block
+            switch(direction){
+                case 'N':
+                    Console.WriteLine("Going North...");
+                    y--;
+                    
+                    break;
+                case 'S':
+                    Console.WriteLine("Going South...");
+                    y++;
+                    break;
+                case 'E':
+                    Console.WriteLine("Going East...");
+                    x--;
+                    break;
+                case 'W':
+                    Console.WriteLine("Going West...");
+                    x++;
+                    break;
+            }
+            int terrain = TerrainChecker();
+            StoryGeneration(terrain);
+            
+        }
+    }
+}
---
+++ MovementOn2DPlanesAndChangingStoryGeneration/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MovementOn2DPlanesAndChangingStoryGeneration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Simulation...\n");
+            //Create a 2D plane. For each increment/decrement on the X or Y axis, have a random story.
+            //For specific places, give a predefined area story and options to interact with the surroundings.
+            //Hold stories in a file
+            //Use enumerable types for terrain. Have different stories for different terrains.
+
+            MovementPlane GameBoard = new MovementPlane();
+            GameBoard.BoardGeneration("Terqia");
+            Console.WriteLine("You open your eyes to the bright blue sky. How did you get here? You get up, inspecting your surroundings.\nWhat direction do you go?");
+            GameBoard.Movement('N');
+            Console.ReadLine();
+        }
+    }
+}
